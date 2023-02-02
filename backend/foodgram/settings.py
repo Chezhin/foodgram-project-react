2712dec5864b626b7590faecee3f32a8db9a0a51
Руслан Чежин <4ezhin@gmail.com>
2023-02-02 23:42:11 +0300

    third review

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 STATIC_URL = '/static/django/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/django/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.CustomUser'
